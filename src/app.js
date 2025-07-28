cef98e2769205210293d8005e74fbd185d9fcf7d
caohaoyang <j.wors@outlook.com>
2025-07-29 00:10:53 +0800

    feat: 提交

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 import express from 'express'
-import { config } from 'dotenv'
+import connectDB from './config/db'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import router from './routes'
 
 const app = express()
 
-app.use(router)
 
 
 // 连接数据库
@@ -17,8 +16,9 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // 路由
+app.use(router)
 // app.use('/api/users', require('./routes/userRoutes'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PROT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
